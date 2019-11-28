86e948289d1e29df5b8da50cf5bfc7e74451aa86
ak672676 <ak672676@gmail.com>
2019-11-29 04:50:42 +0530

    First Commit for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Luck';
+export class AppComponent implements OnInit {
+  title = "Luck";
+
+  attempts = 0;
+
+  guess1 = 0;
+  guess2 = 0;
+  guess3 = 0;
+
+  message = null;
+  guess = [];
+  result = null;
+  selectedNumber = null;
+  guessNumber = null;
+  luckyAttempt = 0;
+  worngAttempt = 0;
+
+  clickedCard: boolean = false;
+
+  g = null;
+
+  isReady: boolean = false;
+
+  ngOnInit() {
+    this.gamePass();
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  nextPass() {
+    this.isReady = true;
+  }
+  onClick(guess) {
+    this.guessNumber = guess;
+    // console.log("Guess is ->", guess);
+    // console.log("GuessNumber is ->", this.guessNumber);
+    if (guess === this.selectedNumber) {
+      this.luckyAttempt++;
+      this.clickedCard = true;
+      setTimeout(() => this.gamePass(), 1500);
+
+      //this.gamePass();
+    } else {
+      this.worngAttempt++;
+      this.clickedCard = true;
+      setTimeout(() => this.gamePass(), 1500);
+    }
+    this.attempts++;
+    if (this.attempts === 10) {
+      this.setResult();
+      this.message = "Click below to replay";
+    }
+  }
+
+  setResult() {
+    console.log("Result");
+    if (this.luckyAttempt <= 3) this.result = "Bad Luck";
+    else if (this.luckyAttempt <= 6) this.result = "Good Luck";
+    else this.result = "Excellent Luck";
+  }
+
+  gamePass() {
+    this.guess = [];
+    this.clickedCard = false;
+    this.guess.push(this.getRandomInt(0, 100));
+    this.guess.push(this.getRandomInt(0, 100));
+    this.guess.push(this.getRandomInt(0, 100));
+
+    this.selectedNumber = this.guess[this.getRandomInt(0, 3)];
+    this.guess1 = this.guess[0];
+    this.guess2 = this.guess[1];
+    this.guess3 = this.guess[2];
+    // console.log("----------------");
+    // console.log("Selected Number is ->" + this.selectedNumber);
+    // console.log("Array is ->" + this.guess.toString());
+  }
+
+  gameReset() {
+    this.message = null;
+    this.guess = [];
+    this.result = null;
+    this.selectedNumber = null;
+    this.luckyAttempt = 0;
+    this.worngAttempt = 0;
+    this.attempts = 0;
+    this.gamePass();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+//import { MatToolbarModule } from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
